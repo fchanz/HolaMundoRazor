f4b18ee2a48b3914b4e53bb13b0b6cb9a6c16bdd
Freddy Chan <fchanz@live.com>
2011-09-19 23:11:44 -0600

    first commit

---
+++ RazorHolaMundo/RazorHolaMundo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorHolaMundo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewData["CurrentTime"] = DateTime.Now.ToString();
+            return View();
+        }
+
+    }
+}
